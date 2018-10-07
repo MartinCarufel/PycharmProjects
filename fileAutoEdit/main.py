0836c3390cb5b6fd6b2ac487368b5100d8d7ef4b
maccam6@gmail.com <maccam6@gmail.com>
2018-10-07 00:25:15 -0400

    add import selection

--- fileAutoEdit/main.py
+++ fileAutoEdit/main.py
@@ -18,18 +18,18 @@ with open(path) as f:
     content = f.readline()
     content = content.strip('"",""\n')
     while content:
-        print(content)
+        #print(content)
         found = False
 
         for i in martin_anne:
             if i in content:
                 if "4540330755072014" in content:
                     outputFile.write(content + ",m," + '\n')
-                    print("Depense Martin")
+                    #print("Depense Martin")
                     found = True
                 if "4540330755072022" in content:
                     outputFile.write(content + ",a," + '\n')
-                    print("Depense Anne-Marie")
+                    #print("Depense Anne-Marie")
                     found = True
                 #break
             else:
@@ -37,12 +37,25 @@ with open(path) as f:
         for i in conjoint:
             if i in content:
                 outputFile.write(content + ",c," + '\n')
-                print("Depense Conjoint")
+                #print("Depense Conjoint")
                 found = True
                 #break
             #else:
                 #pass
         if not found:
-            outputFile.write(content + '\n')
+            lineToList = content.split(",")
+            #print(lineToList)
+            if lineToList[0] == 'VISA 4540330755072014"':
+                print("Carte: {} - Martin - {} - {} - {} " .format(lineToList[0],lineToList[3],lineToList[5],lineToList[11]))
+                choice = input("Choix: (m, a, c ) ? ")
+                outputFile.write(content + "," + choice + '\n')
+            elif lineToList[0] == 'VISA 4540330755072022"':
+                print("Carte: {} - Anne-Marie - {} - {} - {} " .format(lineToList[0],lineToList[3],lineToList[5],lineToList[11]))
+                choice = input("Choix: (m, a, c ) ? ")
+                outputFile.write(content + "," + choice + '\n')
+            else:
+                print("else")
+                outputFile.write(content + ","'\n')
+                pass
         content = f.readline()
         content = content.strip('"",""\n')
