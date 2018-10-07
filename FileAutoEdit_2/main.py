b7ab4c70d992b35c4a79591ed0723fb94cbb3110
maccam6@gmail.com <maccam6@gmail.com>
2018-10-07 19:27:22 -0400

    Default Changelist

--- FileAutoEdit_2/main.py
+++ FileAutoEdit_2/main.py
@@ -19,42 +19,92 @@ inputFile = "conciliation_20180924.csv"
 saveFile = "conciliation_20180924_mod.csv"
 outputFile = open(saveFile, "w")
 
-listTag = ['no_carte', 'date', 'achat', 'debit', 'credit', 'total']
+listTag = ['no_carte', 'date', 'achat', 'debit', 'credit', 'total', 'compte']
 
 feuille = []
 xdict = {}
 j = 0
 line2 = []
+
+
+def compte_pour(content):
+    #print(content)
+    found = False
+    for i in martin_anne:
+        if i in content[2]:
+            if "4540330755072014" in content[0]:
+                content.append('m')
+                #print(content)
+                #outputFile.write(content + ",m," + '\n')
+                #print("Depense Martin")
+                found = True
+            if "4540330755072022" in content[0]:
+                content.append('a')
+                #outputFile.write(content + ",a," + '\n')
+                # print("Depense Anne-Marie")
+                found = True
+            # break
+        else:
+            pass
+    for i in conjoint:
+        if i in content[2]:
+            content.append('c')
+            #outputFile.write(content + ",c," + '\n')
+            # print("Depense Conjoint")
+            found = True
+            # break
+        # else:
+        # pass
+    if not found:
+        content.append('n')
+        # lineToList = content.split(",")
+        # # print(lineToList)
+        # if lineToList[0] == 'VISA 4540330755072014"':
+        #     print("Carte: {} - Martin - {} - {} - {} ".format(lineToList[0], lineToList[3], lineToList[5],
+        #                                                       lineToList[11]))
+        #     choice = input("Choix: (m, a, c ) ? ")
+        #     outputFile.write(content + "," + choice + '\n')
+        # elif lineToList[0] == 'VISA 4540330755072022"':
+        #     print("Carte: {} - Anne-Marie - {} - {} - {} ".format(lineToList[0], lineToList[3], lineToList[5],
+        #                                                           lineToList[11]))
+        #     choice = input("Choix: (m, a, c ) ? ")
+        #     outputFile.write(content + "," + choice + '\n')
+        # else:
+        #     print("else")
+        #     outputFile.write(content + ","'\n')
+        #     pass
+    #print(content)
+    return content
+
+
+
 with open(inputFile, 'r') as f, open('output.csv', 'w') as fo:
     for line in f:
         line = line.replace('"', '').replace("'", "").split(',')
-        lineOut = line[0] + ',' + line[3] + ',' + line[5] + ',' + line[10] + ',' + line[11] + ',' + line[12] + ',' + line[13]
+        lineOut = line[0] + ',' + line[3] + ',' + line[5] + ',' + line[10] + ',' + line[11] + ','
         line2 = lineOut.split(',')
-        fo.write(lineOut)
+        line2 = compte_pour(line2)
+        #print(lineOut)
+
+        #fo.write(lineOut)
         for i in range(len(listTag)):
+            #print(line2)
             xdict[listTag[i]] = line2[i]
             #print(xdict)
 
-        #print(xdict)
+        print(xdict)
         feuille.append(xdict.copy())
 
-print(feuille[i]['no_carte'])
-print(type(feuille[i]['no_carte']))
-
-
-
 root = Tk()
-var = StringVar()
-var.set('hello')
 
+var = StringVar()
 vCarte  = StringVar()
 vDate   = StringVar()
 vAchat  = StringVar()
 vDebit  = StringVar()
 vCredit = StringVar()
 vTotal  = StringVar()
-
-Label(root, textvariable = var)
+vcompte = StringVar()
 
 
 lCarte = Label(root, textvariable=vCarte)
@@ -63,23 +113,76 @@ lAchat = Label(root, textvariable=vAchat)
 lDebit = Label(root, textvariable=vDebit)
 lCredit = Label(root, textvariable=vCredit)
 lTotal = Label(root, textvariable=vTotal)
+lcompte = Label(root, textvariable=vcompte)
+label_radio = Label(root)
 
 
+def sel():
+    selection = var.get()
+    label_radio.config(text = selection)
+
+
+def affiche_radio_button(compte):
+    R1 = Radiobutton(root, text="Martin", variable=var, value='m', command=sel)
+    R1.grid(column=2, columnspan=4, row=2)
+
+    R2 = Radiobutton(root, text="Anne-Marie", variable=var, value='a', command=sel)
+    R2.grid(column=2, columnspan=4, row=3)
+
+    R3 = Radiobutton(root, text="Conjoint", variable=var, value='c', command=sel)
+    R3.grid(column=2, columnspan=4, row=4)
+
+    R4 = Radiobutton(root, text="deux", variable=var, value='d', command=sel)
+    R4.grid(column=2, columnspan=4, row=5)
+
+    R5 = Radiobutton(root, text="non selectionner", variable=var, value='n', command=sel)
+    R5.grid(column=2, columnspan=4, row=6)
+
+
+j=0
 for i in feuille:
+    var = tkinter.StringVar(None, i['compte'])
     vCarte.set(i['no_carte'])
     vDate.set(i['date'])
     vAchat.set(i['achat'])
     vDebit.set(i['debit'])
     vCredit.set(i['credit'])
     vTotal.set(i['total'])
-    lCarte.grid(column=0, row=0)
-    lDate.grid(column=1, row=0)
-    lAchat.grid(column=2, row=0)
-    lDebit.grid(column=3, row=0)
-    lCredit.grid(column=4, row=0)
-    lTotal.grid(column=5, row=0)
+    vcompte.set(i['compte'])
+    lCarte.grid(column=0, row=j)
+    lDate.grid(column=1, row=j)
+    lAchat.grid(column=2, row=j)
+    lDebit.grid(column=3, row=j)
+    lCredit.grid(column=4, row=j)
+    lTotal.grid(column=5, row=j)
+    lcompte.grid(column=6, row=j)
+    affiche_radio_button(i['compte'])
+    but = Button(root, text='Send')
+    but.grid(column=2, columnspan=4, row=7)
+    but.wait_variable(var)
+
     root.update_idletasks()
+    j += 1
+    sleep(.2)
 
-    sleep(1)
+#
+# j=0
+# for i in feuille:
+#     lCarte = Label(root, text=i['no_carte'])
+#     lDate = Label(root, text=i['date'])
+#     lAchat = Label(root, text=i['achat'])
+#     lDebit = Label(root, text=i['debit'])
+#     lCredit = Label(root, text=i['credit'])
+#     lTotal = Label(root, text=i['total'])
+#     lCarte.grid(column=0, row=j)
+#     lDate.grid(column=1, row=j)
+#     lAchat.grid(column=2, row=j)
+#     lDebit.grid(column=3, row=j)
+#     lCredit.grid(column=4, row=j)
+#     lTotal.grid(column=5, row=j)
+#     root.update_idletasks()
+#     j += 1
+#     sleep(.1)
 
 
+#root.mainloop()
