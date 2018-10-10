66cc4876b013b4d211c4ae61919776b0d859f1a0
maccam6@gmail.com <maccam6@gmail.com>
2018-10-09 22:37:43 -0400

    add radio not complet

--- FileAutoEdit_2/main.py
+++ FileAutoEdit_2/main.py
@@ -6,6 +6,38 @@ import os
 import shutil
 import tkinter.filedialog
 import csv
+
+
+
+class Ask_compte(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+        self.choix = StringVar()
+        self.choix.set(value='n')
+        self.geometry('400x200')
+        self.frame = Frame(self)
+        self.label = Label(text="Un petite fenetre")
+        self.closeButton = Button(self, text='Close', command=self.quit)
+        self.frame.pack()
+        self.label.pack()
+        self.rb1 = Radiobutton(text='Martin', variable=self.choix, value='m')
+        self.rb2 = Radiobutton(text='Anne-Marie', variable=self.choix, value='a')
+        self.rb3 = Radiobutton(text='Conjoint', variable=self.choix, value='c')
+        self.rb1.pack()
+        self.rb2.pack()
+        self.rb3.pack()
+        self.closeButton.pack()
+
+
+
+
+
+
+
+
+
+
+
 conjoint = ["IGA", "METRO", "BELL CANADA", "RENO DEPOT", "RONA", "NETFLIX.COM", "LE LUNCH", "MICROSOFT"]
 martin_anne = ["HARVEYS", "JUMBO RESTAURANT", "VIRGIN MOBILE", "ULTRAMAR", "SUBWAY", "MCDONALD", "DOMINOS", "CHEZ GERARD",
                "AU COQ", "SCORES", "BARBIES", "RTM - ABONNEMENT", "A&W", "PACINI", 'LES DELICES', "NICKELS", "SHELL",
@@ -58,21 +90,26 @@ def compte_pour(content):
     if not found:
         content.append('n')
         # lineToList = content.split(",")
-        # # print(lineToList)
-        # if lineToList[0] == 'VISA 4540330755072014"':
-        #     print("Carte: {} - Martin - {} - {} - {} ".format(lineToList[0], lineToList[3], lineToList[5],
-        #                                                       lineToList[11]))
-        #     choice = input("Choix: (m, a, c ) ? ")
-        #     outputFile.write(content + "," + choice + '\n')
-        # elif lineToList[0] == 'VISA 4540330755072022"':
-        #     print("Carte: {} - Anne-Marie - {} - {} - {} ".format(lineToList[0], lineToList[3], lineToList[5],
-        #                                                           lineToList[11]))
-        #     choice = input("Choix: (m, a, c ) ? ")
-        #     outputFile.write(content + "," + choice + '\n')
-        # else:
-        #     print("else")
-        #     outputFile.write(content + ","'\n')
-        #     pass
+        print(content)
+        if content[0] == 'VISA 4540330755072014':
+            print("Carte: {} - Martin - {} - {} - {} ".format(content[0], content[1], content[2],
+                                                              content[4]))
+            # choice = input("Choix: (m, a, c ) ? ")
+            popup = Ask_compte()
+            popup.mainloop()
+
+            # outputFile.write(content + "," + choice + '\n')
+        elif content[0] == 'VISA 4540330755072022':
+            print("Carte: {} - Anne-Marie - {} - {} - {} ".format(content[0], content[1], content[2],
+                                                                  content[4]))
+            popup = Ask_compte()
+            popup.mainloop()
+            # choice = input("Choix: (m, a, c ) ? ")
+            # outputFile.write(content + "," + choice + '\n')
+        else:
+            print("else")
+            #outputFile.write(content + ","'\n')
+            pass
     #print(content)
     return content
 
@@ -92,78 +129,80 @@ with open(inputFile, 'r') as f, open('output.csv', 'w') as fo:
             xdict[listTag[i]] = line2[i]
             #print(xdict)
 
-        print(xdict)
+        #print(xdict)
         feuille.append(xdict.copy())
 
-root = Tk()
 
-var = StringVar()
-vCarte  = StringVar()
-vDate   = StringVar()
-vAchat  = StringVar()
-vDebit  = StringVar()
-vCredit = StringVar()
-vTotal  = StringVar()
-vcompte = StringVar()
-
-
-lCarte = Label(root, textvariable=vCarte)
-lDate =  Label(root, textvariable=vDate)
-lAchat = Label(root, textvariable=vAchat)
-lDebit = Label(root, textvariable=vDebit)
-lCredit = Label(root, textvariable=vCredit)
-lTotal = Label(root, textvariable=vTotal)
-lcompte = Label(root, textvariable=vcompte)
-label_radio = Label(root)
-
-
-def sel():
-    selection = var.get()
-    label_radio.config(text = selection)
-
-
-def affiche_radio_button(compte):
-    R1 = Radiobutton(root, text="Martin", variable=var, value='m', command=sel)
-    R1.grid(column=2, columnspan=4, row=2)
-
-    R2 = Radiobutton(root, text="Anne-Marie", variable=var, value='a', command=sel)
-    R2.grid(column=2, columnspan=4, row=3)
-
-    R3 = Radiobutton(root, text="Conjoint", variable=var, value='c', command=sel)
-    R3.grid(column=2, columnspan=4, row=4)
-
-    R4 = Radiobutton(root, text="deux", variable=var, value='d', command=sel)
-    R4.grid(column=2, columnspan=4, row=5)
-
-    R5 = Radiobutton(root, text="non selectionner", variable=var, value='n', command=sel)
-    R5.grid(column=2, columnspan=4, row=6)
-
-
-j=0
-for i in feuille:
-    var = tkinter.StringVar(None, i['compte'])
-    vCarte.set(i['no_carte'])
-    vDate.set(i['date'])
-    vAchat.set(i['achat'])
-    vDebit.set(i['debit'])
-    vCredit.set(i['credit'])
-    vTotal.set(i['total'])
-    vcompte.set(i['compte'])
-    lCarte.grid(column=0, row=j)
-    lDate.grid(column=1, row=j)
-    lAchat.grid(column=2, row=j)
-    lDebit.grid(column=3, row=j)
-    lCredit.grid(column=4, row=j)
-    lTotal.grid(column=5, row=j)
-    lcompte.grid(column=6, row=j)
-    affiche_radio_button(i['compte'])
-    but = Button(root, text='Send')
-    but.grid(column=2, columnspan=4, row=7)
-    but.wait_variable(var)
-
-    root.update_idletasks()
-    j += 1
-    sleep(.2)
+#
+# root = Tk()
+#
+# var = StringVar()
+# vCarte  = StringVar()
+# vDate   = StringVar()
+# vAchat  = StringVar()
+# vDebit  = StringVar()
+# vCredit = StringVar()
+# vTotal  = StringVar()
+# vcompte = StringVar()
+#
+#
+# lCarte = Label(root, textvariable=vCarte)
+# lDate =  Label(root, textvariable=vDate)
+# lAchat = Label(root, textvariable=vAchat)
+# lDebit = Label(root, textvariable=vDebit)
+# lCredit = Label(root, textvariable=vCredit)
+# lTotal = Label(root, textvariable=vTotal)
+# lcompte = Label(root, textvariable=vcompte)
+# label_radio = Label(root)
+#
+#
+# def sel():
+#     selection = var.get()
+#     label_radio.config(text=selection)
+#
+#
+# def affiche_radio_button(compte):
+#     R1 = Radiobutton(root, text="Martin", variable=var, value='m', command=sel)
+#     R1.grid(column=2, columnspan=4, row=2)
+#
+#     R2 = Radiobutton(root, text="Anne-Marie", variable=var, value='a', command=sel)
+#     R2.grid(column=2, columnspan=4, row=3)
+#
+#     R3 = Radiobutton(root, text="Conjoint", variable=var, value='c', command=sel)
+#     R3.grid(column=2, columnspan=4, row=4)
+#
+#     R4 = Radiobutton(root, text="deux", variable=var, value='d', command=sel)
+#     R4.grid(column=2, columnspan=4, row=5)
+#
+#     R5 = Radiobutton(root, text="non selectionner", variable=var, value='n', command=sel)
+#     R5.grid(column=2, columnspan=4, row=6)
+#
+#
+# j = 0
+# for i in feuille:
+#     var = tkinter.StringVar(None, i['compte'])
+#     vCarte.set(i['no_carte'])
+#     vDate.set(i['date'])
+#     vAchat.set(i['achat'])
+#     vDebit.set(i['debit'])
+#     vCredit.set(i['credit'])
+#     vTotal.set(i['total'])
+#     vcompte.set(i['compte'])
+#     lCarte.grid(column=0, row=j)
+#     lDate.grid(column=1, row=j)
+#     lAchat.grid(column=2, row=j)
+#     lDebit.grid(column=3, row=j)
+#     lCredit.grid(column=4, row=j)
+#     lTotal.grid(column=5, row=j)
+#     lcompte.grid(column=6, row=j)
+#     affiche_radio_button(i['compte'])
+#     but = Button(root, text='Send')
+#     but.grid(column=2, columnspan=4, row=7)
+#     but.wait_variable(var)
+#
+#     root.update_idletasks()
+#     j += 1
+#     sleep(.2)
 
 #
 # j=0
