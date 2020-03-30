3c1948e4990674e3002d2a159d23c9536b73a0a4
martin Carufel <martin.carufel@directed.com>
2020-03-30 09:32:04 -0400

    new structure work now

--- logic_sim/pin.py
+++ logic_sim/pin.py
@@ -1,15 +1,1 @@
-class Pin:
-
-    # __init__ function
-    def __init__(self):
-        self.pin = {}
-        # Function to add key:value
-    staticmethod
-    def add(self, key, value):
-        self.pin[key] = value
-
-    def change(self, key, value):
-        if key in self.pin:
-            self.pin[key] = value
-        else:
-            print('{} no data found'.format(key))
+pin = {}
--- logic_sim/test.py
+++ logic_sim/test.py
@@ -1,40 +1,29 @@
+import pin
 import ttl_7408
-import switch_2
-import connection_2 as con
 import input
+import switch
+import connection as w
 from time import sleep
 
-u1 = ttl_7408.TTL_7408()
-# u2 = ttl_7408.TTL_7408()
-in1 = input.Input()
-in2 = input.Input()
-in3 = input.Input()
+u1 = ttl_7408.TTL_7408('u1')
+in1 = input.Input('in1')
+in2 = input.Input('in2')
+sw1 = switch.Switch('sw1')
+w1 = w.Connection('in1out', 'sw1a1')
+w2 = w.Connection('in2out', 'u1b1')
+w3 = w.Connection('sw1y1', 'u1a1')
+# print(pin.pin)
+
+
 in1.set('out', 1)
 in2.set('out', 1)
-in3.set('out', 0)
-sw1 = switch_2.Switch()
-# sw1.set_state(True)
+sw1.set_state(True)
 
-# w4 = con.Connection(in1.get('out'), u1, 'a2')
-# w5 = con.Connection(in3.get('out'), u1, 'b2')
-# w6 = con.Connection(u1.get('y2'), u1, 'b1')
+# print(pin.pin)
 
 
-w1 = con.Connection(in1.get('out'), u1, 'a1')
-w2 = con.Connection(sw1.get('y1'), u1, 'b1')
-# w2 = con.Connection(1, u1, 'b1')
-w3 = con.Connection(in2.get('out'), sw1, 'a1')
-
-
-
-sleep(0.5)
-#
-# print('sw1 pin in: %s, out: %s' %(sw1.get('in'), sw1.get('out')))
-# print('w3 pin in: %s, out: %s' %(w3.get('in'), w3.get('out')))
-# print('w2 pin in: %s, out: %s' %(w2.get('in'), w2.get('out')))
-#
-#
-# print('u1 pin a1: %s, b1: %s' %(u1.get('a1'), u1.get('b1')))
-# print('u1 y1: %s' %(u1.get('y1')))
-print('u1 y1: %s' %(u1.get('y1')))
-
+# u1.set('a1', 1)
+# u1.set('b1', 1)
+sleep(1)
+print(u1.get('y1'))
+print(pin.pin)
