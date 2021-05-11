8cd78b84fdc9394b595f0a0f540fc317d74e71bc
Martin C <maccam6@gmail.com>
2021-05-11 14:16:26 -0400

    Change progression indicator.

--- USB_stress_test_log_clean_up/main.py
+++ USB_stress_test_log_clean_up/main.py
@@ -1,7 +1,8 @@
 #coding:utf-8
 
 import tkinter.filedialog
-
+from time import sleep
+from os import system
 
 def main():
     data = []
@@ -9,6 +10,7 @@ def main():
     path = tkinter.filedialog.askopenfilename()
     f = open(path, mode='r')
     indice = 0
+    progress = []
     for line in f:
         if "START STRESS TEST" in line or "Thread started: USBStreamThread" in line:
             log_data = True
@@ -17,22 +19,26 @@ def main():
 
         if log_data:
             data.append(line)
-        if indice % 100000 == 0:
-            print('.', end='')
+
+        if (indice % 50000) == 0:
+            progress.append(".")
+            _ = system('cls')
+            print("".join(progress))
         indice += 1
 
-    f.close()
-
     with open(path[:-4] + "_clean" + ".txt", mode='w') as wf:
+        progress = []
         indice = 0
         for i in data:
             wf.writelines(i)
-            if indice % 1000 == 0:
-                print('.', end='')
+            if (indice % 5000) == 0:
+                progress.append(".")
+                _ = system('cls')
+                print("".join(progress))
+
             indice += 1
-
-
-
+    filename = path[:-4] + "_clean" + ".txt"
+    print("File created: ", filename)
 
 if __name__ == "__main__":
     main()
