0b4aba48e08ad19e6533054c7082cf44bb0db0e6
Martin C <maccam6@gmail.com>
2021-05-27 20:17:43 -0400

    working version

--- spectrum_analyser/main.py
+++ spectrum_analyser/main.py
@@ -18,9 +18,10 @@ def main():
             value_avg.append(df[col].mean())
             # avg_dba[col] = df[col].mean()
         # print(df[col])
-    avg_df = pd.DataFrame({'Frequency':freq_list[:], 'dbA':value_avg[:]})
+    avg_df = pd.DataFrame({'Frequency': freq_list[:], 'dBA': value_avg[:]})
+    # avg_df = pd.DataFrame({'Frequency': ['10 Hz', '20 Hz', '30 Hz'], 'dBA': [5, 10, 15]})
     print(avg_df)
-    graph = df.plot.bar(x='Frequency', y='dbA', rot=0)
+    graph = avg_df.plot.bar(x='Frequency', y='dBA')
     # plt.plot(avg_df)
 
     plt.show()
