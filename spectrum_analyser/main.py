7dab2bcd2ec42c063ef3d5cc63bf11fbe4384551
MartinCarufel <maccam6@gmail.com>
2021-05-28 18:18:00 -0400

    add figure save to file

--- spectrum_analyser/main.py
+++ spectrum_analyser/main.py
@@ -4,6 +4,14 @@ from tkinter import filedialog
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def creat_fig_file_name(path):
+    path_list = path.split('/')
+    file_name = path_list[-1][:-4]
+    return file_name
+    print(file_name)
+    # print(path_list)
+
+
 def main():
     print("Hello world")
     avg_dba = {}
@@ -20,10 +28,15 @@ def main():
         # print(df[col])
     avg_df = pd.DataFrame({'Frequency': freq_list[:], 'dBA': value_avg[:]})
     # avg_df = pd.DataFrame({'Frequency': ['10 Hz', '20 Hz', '30 Hz'], 'dBA': [5, 10, 15]})
-    print(avg_df)
-    graph = avg_df.plot.bar(x='Frequency', y='dBA')
+    # print(avg_df)
+
+    # plt.show()/
+
+    graph = avg_df.plot.bar(x='Frequency', y='dBA', figsize=(11, 6),  )
     # plt.plot(avg_df)
 
+    creat_fig_file_name(path_file.name)
+    plt.savefig(str(creat_fig_file_name(path_file.name)) + ".pdf", bbox_inches='tight')
     plt.show()
 
     # print(avg_dba)
