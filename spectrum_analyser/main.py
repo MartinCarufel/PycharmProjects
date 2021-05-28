bd80ae4474050f451368ebe7b7aa2380462a7c6c
MartinCarufel <maccam6@gmail.com>
2021-05-28 19:08:54 -0400

    refactor with function

--- spectrum_analyser/main.py
+++ spectrum_analyser/main.py
@@ -4,43 +4,35 @@ from tkinter import filedialog
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def creat_fig_file_name(path):
+
+def create_fig_file_name(path):
     path_list = path.split('/')
     file_name = path_list[-1][:-4]
     return file_name
     print(file_name)
-    # print(path_list)
 
+def create_plot(avg_df, path_file):
+    avg_df.plot.bar(x='Frequency', y='dBA', figsize=(11, 6), )
+    create_fig_file_name(path_file.name)
+    plt.savefig(str(create_fig_file_name(path_file.name)) + ".pdf", bbox_inches='tight')
+    plt.show()
 
-def main():
-    print("Hello world")
-    avg_dba = {}
+def create_avg_data(path_file):
+    # avg_dba = {}
     freq_list = []
     value_avg = []
-    path_file = filedialog.askopenfile()
     df = pd.read_table(path_file, skiprows=8)
     for col in df:
         if col.endswith("Hz"):
-            # print(col)
             freq_list.append(col)
             value_avg.append(df[col].mean())
-            # avg_dba[col] = df[col].mean()
-        # print(df[col])
     avg_df = pd.DataFrame({'Frequency': freq_list[:], 'dBA': value_avg[:]})
-    # avg_df = pd.DataFrame({'Frequency': ['10 Hz', '20 Hz', '30 Hz'], 'dBA': [5, 10, 15]})
-    # print(avg_df)
+    return avg_df
 
-    # plt.show()/
-
-    graph = avg_df.plot.bar(x='Frequency', y='dBA', figsize=(11, 6),  )
-    # plt.plot(avg_df)
-
-    creat_fig_file_name(path_file.name)
-    plt.savefig(str(creat_fig_file_name(path_file.name)) + ".pdf", bbox_inches='tight')
-    plt.show()
-
-    # print(avg_dba)
-
+def main():
+    path_file = filedialog.askopenfile()
+    avg_df = create_avg_data(path_file)
+    create_plot(avg_df, path_file)
 
 
 if __name__ == '__main__':
