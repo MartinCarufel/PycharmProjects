0e79ba9b8205821a7d3c1e9ee1dc8529f885a0d0
Martin Carufel <maccam6@gmail.com>
2021-06-10 16:29:34 -0400

    level 2 clean

---
+++ USB_stress_test_log_clean_up/main2.py
@@ -0,0 +1,45 @@
+#coding:utf-8
+
+import tkinter.filedialog
+from time import sleep
+from os import system
+import re
+
+
+def main():
+    data = []
+    log_data = False
+    path = tkinter.filedialog.askopenfilename()
+    f = open(path, mode='r')
+    indice = 0
+    progress = []
+    lookup_string = "|    490"
+
+    for line in f:
+
+        if line.startswith(lookup_string):
+            data.append(line)
+
+        if (indice % 50000) == 0:
+            progress.append(".")
+            _ = system('cls')
+            print("".join(progress))
+        indice += 1
+
+    with open(path[:-4] + "_clean_lvl2" + ".txt", mode='w') as wf:
+        progress = []
+        indice = 0
+        for i in data:
+            wf.writelines(i)
+            if (indice % 5000) == 0:
+                progress.append(".")
+                _ = system('cls')
+                print("".join(progress))
+
+            indice += 1
+    filename = path[:-4] + "_clean_lvl2" + ".txt"
+    print("File created: ", filename)
+
+
+if __name__ == "__main__":
+    main()
---
+++ USB_stress_test_log_clean_up/run2.bat
@@ -0,0 +1,2 @@
+python main2.py
+pause
