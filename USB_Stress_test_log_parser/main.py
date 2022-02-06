bd6a513f139f362f33ef530562ac726d1bee942b
Martin C <maccam6@gmail.com>
2022-02-06 09:38:46 -0500

    Major update, refactor the prog  and unittest

--- USB_Stress_test_log_parser/main.py
+++ USB_Stress_test_log_parser/main.py
@@ -3,88 +3,178 @@ import re
 import sys
 import os
 
+
 def Average(lst):
     return sum(lst) / len(lst)
 
-param_file_name = str(sys.argv[1])
-hp_serial = param_file_name[15:-4]
-# print("---------------")
-# print(hp_serial)
-# print("---------------")
-pd.set_option('display.max_rows', 20)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', None)
 
-match_data = "\| +[0-9]+.[0-9][0-9]"
-match_pattern = "\| +"
+def prog_setup():
+    param_file_name = str(sys.argv[1])
+    reg_ex = 'IO-[0-9][0-9]-[0-9][0-9][0-9][0-9][0-9][0-9]'    #Find in path/file the IO serial
+    hp_serial = re.search(pattern=reg_ex, string=param_file_name).group()
+    return (param_file_name, hp_serial)
+
+
+def extract_stress_test_data(input_file):
+    """
+    This function get in input the file output log from the USB Stress test in format text
+    and convert only the test result data second per second into an two dim list of CSV string. Each column are split
+    with comma.
+
+    input_file: usb stress test log.txt
+    output: list of a list of string data
+
+    ex on input file
+    +--------------------------------------------------------------------------------------+
+    | START
+    STRESS
+    TEST |
+    | |
+    | Date: 02 / 02 / 22
+    12: 21:53 |
+    ...
+    +----------+---------------+----------------+----------------+----------+----------+----------+--------+--------------+--------------+--------------+--------------+
+    | duration |  # total frames | #total bad pkt | #total dropped | #frames  | #dropped | avg. fps |  MB/s  | #C0 Dead img | #C1 Dead img | #C2 Dead img | #C3 Dead img |
+    +----------+---------------+----------------+----------------+----------+----------+----------+--------+--------------+--------------+--------------+--------------+
+    Warm - Up 2.00 seconds...
+
+    *****Data that will be collected***
+    | 1.00 | 30 | 0 | 0 | 30 | 0 | 30.00 | 295.42 | 0 | 0 | 0 | 0 |
+    | 2.00 | 60 | 0 | 0 | 30 | 0 | 30.00 | 295.37 | 0 | 0 | 0 | 0 |
+    | 3.00 | 90 | 0 | 0 | 30 | 0 | 30.00 | 295.49 | 0 | 0 | 0 | 0 |
+    ....
+    """
+
+    match_data = "\| +[0-9]+.[0-9][0-9]"    # Regex to find the test result data to filter out any other text
+    match_pattern = "\| +"                  # Regex to find all separating character between to text column
+    csv_data = []
+
+    with open(input_file, mode='r') as f:
+        for line in f:
+            if re.match(match_data, line) != None:
+                new_line = re.sub(pattern=match_pattern, repl=",", string=line)[1:-2]
+                csv_data.append(new_line.split(sep=','))
+    return csv_data
+
 header = ["Duration", "# Total Frame", "# total bad pkt",  "# total dropped", "# frames",
             "# dropped", "avg. fps", "MB/s", "#C0 Dead img", "#C1 Dead img", "#C2 Dead img", "#C3 Dead img"]
-csv_data = []
-with open(param_file_name, mode='r') as f:
-    for line in f:
-        if re.match(match_data, line) != None:
-            new_line = re.sub(pattern=match_pattern, repl=",", string=line)[1:-2]
-            # print(new_line)
-            csv_data.append(new_line.split(sep=','))
-            # csv_data.append(re.split(pattern=match_pattern, string=line))
-            # csv_data.append(new_line)
 
-# for i in csv_data:
-#     print(i)
-# print(csv_data)
+def convert_listcsv_to_dataframe(csv_data, hp_serial="xxxxxxx"):
+    """
+    This function take as input a list of list of string extracted from the USB Stress
+    test and convert it to a dataframe.
+     """
 
-df = pd.DataFrame(csv_data, columns=header)
-df["Duration"] = df["Duration"].astype(float)
-df["avg. fps"] = df["avg. fps"].astype(float)
-df["# total dropped"] = df["# total dropped"].astype(int)
-# print(df["avg. fps"].mean())
-# print(df)
-# df.info()
-total_drop = []
-avg_fps_list = []
-avg_fps = []
-nb_line, nb_col = df.shape
-for i in range(0, nb_line):
-    if i == 0:
-        avg_fps.append(df["avg. fps"].iloc[i])
-    elif float(df["Duration"].iloc[i]) > float(df["Duration"].iloc[i-1]):
-        avg_fps.append(df["avg. fps"].iloc[i])
+    header = ["Duration", "# Total Frame", "# total bad pkt", "# total dropped", "# frames",
+              "# dropped", "avg. fps", "MB/s", "#C0 Dead img", "#C1 Dead img", "#C2 Dead img", "#C3 Dead img"]
+    df = pd.DataFrame(csv_data, columns=header)
+    df["Duration"] = df["Duration"].astype(float)
+    df["avg. fps"] = df["avg. fps"].astype(float)
+    df["# total dropped"] = df["# total dropped"].astype(int)
+    # print(df["avg. fps"].mean())
+    # print(df)
+    # df.info()
+    df.to_excel("Export_data " + hp_serial + ".xlsx")
+    return df
+
+def compile_test_data_per_minute(df, hp_serial="xxxxxxx"):
+    total_drop = []
+    avg_fps_list = []
+    avg_fps = []
+    nb_line, nb_col = df.shape
+    for i in range(0, nb_line):
+        if i == 0:
+            avg_fps.append(df["avg. fps"].iloc[i])
+        elif float(df["Duration"].iloc[i]) > float(df["Duration"].iloc[i-1]):
+            avg_fps.append(df["avg. fps"].iloc[i])
+        else:
+            total_drop.append(df["# total dropped"].iloc[i])
+            avg_fps_list.append(Average(avg_fps))
+            avg_fps = []
+            avg_fps.append(df["avg. fps"].iloc[i])
+
+
+    # df.to_excel("Export_data " + hp_serial +".xlsx")
+    df_summary = pd.DataFrame()
+    df_summary["Total Drop Frame"] = total_drop
+    df_summary["Total Drop Frame"] = df_summary["Total Drop Frame"].astype(int)
+    df_summary["Avg FPS"] = avg_fps_list
+    df_summary["Avg FPS"] = df_summary["Avg FPS"].round(decimals=3)
+    df_summary.to_excel("Export_summary " + hp_serial +".xlsx")
+    # print(total_drop)
+    # print(avg_fps_list)
+    # print(df_summary)
+    # print(df_summary.dtypes)
+    return df_summary
+
+
+def create_test_result_summary_csv(hp_serial, df_summary):
+    """
+    This function create a CSV file that summarize for the given HP the total of drop frame and the average FPS
+    for each thread
+    :param hp_serial:
+    :param df_summary:
+    :return:
+    """
+    if not os.path.exists("Data_summary.csv"):
+        data_summary = open("Data_summary.csv", mode='w')
+        data_summary.close()
+
+    if os.path.getsize("Data_summary.csv") == 0:
+        data_summary = open("Data_summary.csv", mode='w')
+        data_summary.writelines("hp serial,Thread,Total Drop Frame,Avg FPS\n")
+        print('File Data_summary.csv created\n')
+        for index, row in df_summary.iterrows():
+            data_summary.write(hp_serial + ',' + str(int(index)+1) + ',' + str(int((row["Total Drop Frame"]))) + ',' + str(row["Avg FPS"]) + ',')
+            data_summary.write("\n")
+        data_summary.close()
     else:
-        total_drop.append(df["# total dropped"].iloc[i])
-        avg_fps_list.append(Average(avg_fps))
-        avg_fps = []
-        avg_fps.append(df["avg. fps"].iloc[i])
+        data_summary = open("Data_summary.csv", mode='a')
+        for index, row in df_summary.iterrows():
+            data_summary.write(hp_serial + ',' + str(int(index)+1) + ',' + str(int((row["Total Drop Frame"]))) + ',' + str(row["Avg FPS"]) + ',')
+            data_summary.write("\n")
+        data_summary.close()
+        print('File Data_summary.csv updated\n')
 
 
-# df.to_excel("Export_data " + hp_serial +".xlsx")
-df_sumarry = pd.DataFrame()
-df_sumarry["Total Drop Frame"] = total_drop
-df_sumarry["Total Drop Frame"] = df_sumarry["Total Drop Frame"].astype(int)
-df_sumarry["Avg FPS"] = avg_fps_list
-df_sumarry["Avg FPS"] = df_sumarry["Avg FPS"].round(decimals=3)
-# df_sumarry.to_excel("Export_summary " + hp_serial +".xlsx")
-# print(total_drop)
-# print(avg_fps_list)
-# print(df_sumarry)
-# print(df_sumarry.dtypes)
 
-if not os.path.exists("Data_summary.csv"):
-    data_summary = open("Data_summary.csv", mode='w')
-    data_summary.close()
+def get_path_list_from_file(file):
+    """
+    Read a text file that contain path\filename and return a list of path and convert DOS '\' with Python '/'
+    :param file:
+    :return: list
+    """
 
-if os.path.getsize("Data_summary.csv") == 0:
-    data_summary = open("Data_summary.csv", mode = 'w')
-    data_summary.writelines("Min 1 Drop Frame,Min 1 Avg FPS,Min 2 Drop Frame,Min 2 Avg FPS,Min 3 Drop Frame,Min 3 Avg FPS,Min 4 Drop Frame,Min 4 Avg FPS,Min 5 Drop Frame,Min 5 Avg FPS,Min 6 Drop Frame,Min 6 Avg FPS,Min 7 Drop Frame,Min 7 Avg FPS,Min 8 Drop Frame,Min 8 Avg FPS,Min 9 Drop Frame,Min 9 Avg FPS,Min 10 Drop Frame,Min 10 Avg FPS\n")
-    for index, row in df_sumarry.iterrows():
-        data_summary.write(str(row["Total Drop Frame"]) + ',' + str(row["Avg FPS"]) + ',')
-    data_summary.write("\n")
-    data_summary.close()
+    with open(file, mode='r') as f:
+        path_list = []
+        for line in f:
+            line = line.replace('\\','/')
+            line = line.replace('\n','')
+            path_list.append(line)
+    return path_list
 
-else:
-    data_summary = open("Data_summary.csv", mode='a')
-    for index, row in df_sumarry.iterrows():
-        # print(int(row["Total Drop Frame"]))
-        data_summary.write(str(int(row["Total Drop Frame"])) + ',' + str(row["Avg FPS"]) + ',')
-    data_summary.write("\n")
-    data_summary.close()
+
+
+if __name__ == '__main__':
+
+    for file in get_path_list_from_file('File_list_to_analyse.txt'):
+        reg_ex = 'IO-[0-9][0-9]-[0-9][0-9][0-9][0-9][0-9][0-9]'  # Find in path/file the IO serial
+        hp_serial = re.search(pattern=reg_ex, string=file).group()
+        print("Process the log for HP {}.".format(hp_serial))
+        csv_data = extract_stress_test_data(file)
+        df = convert_listcsv_to_dataframe(csv_data, hp_serial=hp_serial)
+        summary = compile_test_data_per_minute(df, hp_serial)
+        create_test_result_summary_csv(hp_serial, summary)
+
+    print('DONE !')
+
+
+
+    # file_name = prog_setup()[0]
+    # hp_serial = prog_setup()[1]
+    # csv_data = extract_stress_test_data(file_name)
+    # df = convert_listcsv_to_dataframe(csv_data, hp_serial=hp_serial)
+    # summary = compile_test_data_per_minute(df, hp_serial)
+    # create_test_result_summary_csv(hp_serial, summary)
+
+
