f6d1a148afa16afeb7e05357b2196b2cf95d8807
Martin Carufel <maccam6@gmail.com>
2022-10-20 21:33:46 -0400

    New project

---
+++ auto_USB_stress_test/main.py
@@ -0,0 +1,34 @@
+import subprocess
+import os
+import sys
+import re
+import pandas as pd
+
+sys.path.append("D:/user_data/Martin/OneDrive/Documents/git/PycharmProjects/USB_Stress_test_log_parser")
+import log_parser
+
+
+def main():
+    file = "usb_stress_testIO-04-000979_disc.log"
+    reg_ex = 'IO-[0-9][0-9]-[0-9][0-9][0-9][0-9][0-9][0-9]'  # Find in path/file the IO serial
+    hp_serial = re.search(pattern=reg_ex, string=file).group()
+    os.system("D:/user_data/Martin/Downloads/scanner-io-4.0.1-484-hptest-20201208T135846Z-001/scanner-io-4.0.1-484-hptest/usb_stress_test.exe")
+    print("USB ST finished")
+    usb_error = log_parser.check_for_usb_error_v2(file, hp_serial)
+    extract_data = log_parser.extract_stress_test_data(file)
+    df = log_parser.convert_listcsv_to_dataframe(log_parser.extract_stress_test_data(file))
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    # print(df[df['Duration'].between(13.0, 13.9)]['# total dropped'].max)  # Trouve la valeur max de toutes les lignes
+    fail_flag = log_parser.acceptance_test(df, drop_frame_criteria=7, fps_criteria= 29.95)
+
+    print("Fail code: {}" .format(fail_flag))
+    print("USB Error: {}" .format(usb_error))
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+
--- image_recon/main.py
+++ image_recon/main.py
@@ -6,20 +6,23 @@ from screeninfo import get_monitors
 pyautogui.moveTo(100, 100)
 # pyautogui.click(clicks=1, interval=0.0, button=pyautogui.PRIMARY)
 
+min_y_pos = []
 for m in get_monitors():
-    print(str(m))
+    min_y_pos.append(m.y)
+    # print(str(m))
+print(min(min_y_pos))
 
-# x,y = pyautogui.position()
-# print(x,y)
-#
-# pos = imagesearch("4.png")
-# img = cv2.imread("4.png")
-# print(img.shape[0]/2, img.shape[1]/2)
-# if pos[0] != -1:
-#     print("position : ", pos[0], pos[1])
-#
-#     pyautogui.moveTo(pos[0]+img.shape[0]/2, pos[1]+img.shape[1]/2)
-#     pyautogui.click(clicks=2, interval=0.0, button=pyautogui.PRIMARY)
-#     # click_image("3.png", pos, action="right", timestamp=0.1)
-# else:
-#     print("image not found")
+x,y = pyautogui.position()
+print(x,y)
+
+pos = imagesearch("4b.png")
+img = cv2.imread("4b.png")
+print(img.shape[0]/2, img.shape[1]/2)
+if pos[0] != -1:
+    print("position : ", pos[0], pos[1])
+
+    pyautogui.moveTo(pos[0]+img.shape[0]/2, pos[1]+img.shape[1]/2+min(min_y_pos))
+    pyautogui.click(clicks=2, interval=0.0, button=pyautogui.PRIMARY)
+    # click_image("3.png", pos, action="right", timestamp=0.1)
+else:
+    print("image not found")
