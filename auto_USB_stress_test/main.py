b94e0315e72b00ae80e44c5b34fccb31503559d2
Martin C <maccam6@gmail.com>
2022-10-22 19:11:00 -0400

    file parse

--- auto_USB_stress_test/main.py
+++ auto_USB_stress_test/main.py
@@ -12,25 +12,43 @@ import log_parser
 usb_strest_test_path = "C:/tools/scanner-io-4.0.1-484-hptest/usb_stress_test.exe"
 
 def main():
-    file = "usb_stress_testIO-04-000979_disc.log"
-    reg_ex = 'IO-[0-9][0-9]-[0-9][0-9][0-9][0-9][0-9][0-9]'  # Find in path/file the IO serial
-    hp_serial = re.search(pattern=reg_ex, string=file).group()
-    os.system(usb_strest_test_path)
-    print("USB ST finished")
-    usb_error = log_parser.check_for_usb_error_v2(file, hp_serial)
-    extract_data = log_parser.extract_stress_test_data(file)
-    df = log_parser.convert_listcsv_to_dataframe(log_parser.extract_stress_test_data(file))
-    pd.set_option('display.max_rows', None)
-    pd.set_option('display.max_columns', None)
-    # print(df[df['Duration'].between(13.0, 13.9)]['# total dropped'].max)  # Trouve la valeur max de toutes les lignes
-    fail_flag = log_parser.acceptance_test(df, drop_frame_criteria=7, fps_criteria= 29.95)
+    # file = "usb_stress_testIO-04-000979_disc.log"
+    # reg_ex = 'IO-[0-9][0-9]-[0-9][0-9][0-9][0-9][0-9][0-9]'  # Find in path/file the IO serial
+    # hp_serial = re.search(pattern=reg_ex, string=file).group()
 
-    print("Fail code: {}" .format(fail_flag))
-    print("USB Error: {}" .format(usb_error))
+
+    # os.system(usb_strest_test_path)
+    print("USB ST finished")
+
+    expression = "usb_stress_testIO-[0-9][0-9]-[0-9]{6}.log"
+    dir_content = os.listdir(".")
+    print(dir_content)
+    for id in range(len(dir_content)):
+    # for file in dir_content:
+        result = re.match(expression, dir_content[id])
+        if result != None:
+            print("file found:" .format(dir_content[id]))
+            reg_ex = 'IO-[0-9][0-9]-[0-9][0-9][0-9][0-9][0-9][0-9]'  # Find in path/file the IO serial
+            hp_serial = re.search(pattern=reg_ex, string=dir_content[id]).group()
+            usb_error = log_parser.check_for_usb_error_v2(dir_content[id], hp_serial)
+            # extract_data = log_parser.extract_stress_test_data(str(result))
+            df = log_parser.convert_listcsv_to_dataframe(log_parser.extract_stress_test_data(dir_content[id]))
+            pd.set_option('display.max_rows', None)
+            pd.set_option('display.max_columns', None)
+            print(df[df['Duration'].between(13.0, 13.9)][
+                      '# total dropped'].max)  # Trouve la valeur max de toutes les lignes
+            fail_flag = log_parser.acceptance_test(df, drop_frame_criteria=7, fps_criteria=29.95)
+
+            print("Fail code: {}".format(fail_flag))
+            print("USB Error: {}".format(usb_error))
+            break
+        else:
+            print("Not Found")
+
 
 def test_re():
     import os
-    expression = "usb_stress_testIO-[0-9][0-9]-[0-9]{6}_ok.log"
+    expression = "usb_stress_testIO-[0-9][0-9]-[0-9]{6}.log"
     dir_content = os.listdir(".")
     print(dir_content)
     for file in dir_content:
@@ -43,7 +61,7 @@ def test_re():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    test_re()
-    # main()
+    # test_re()
+    main()
 
 
