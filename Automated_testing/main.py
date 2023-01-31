e39ad4bf43705caffaa7dc21165ffecb517da730
Martin Carufel <maccam6@gmail.com>
2023-01-30 21:37:54 -0500

    automated testing

---
+++ Automated_testing/main.py
@@ -0,0 +1,21 @@
+import pyautogui as pg
+
+
+class Test_suite:
+
+    def __init__(self):
+        self.win_main = {
+            'win logo': 'win_logo.png',
+
+        }
+        pass
+
+    def test1(self):
+        # location = pg.locateCenterOnScreen('win_logo.png', confidence=0.9)
+        # print(location)
+        # pg.moveTo(pg.locateCenterOnScreen('win_logo.png', confidence=0.9))
+        pg.click(pg.locateCenterOnScreen(self.win_main['win logo'], confidence=0.9))
+
+
+t = Test_suite()
+t.test1()
