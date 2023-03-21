76e2e1a5706fb8f2cd3f02ba259d07aef669979c
Martin Carufel <maccam6@gmail.com>
2023-03-21 17:30:26 -0400

    integrate Http get for specification Use config file for many parameter

--- Work_with_json/json_to_pandas.py
+++ Work_with_json/json_to_pandas.py
@@ -6,6 +6,10 @@ from docx import Document
 import re
 import yaml
 import datetime
+import requests
+
+USER = 'martin.carufel@dental-wings.com'
+PASSWORD = '18,Mac&Amo'
 
 pd.set_option("display.max_columns", 5)
 status_text = {1:"Pass",
@@ -79,8 +83,6 @@ def extract_all_step_result(custom_step_result):
     return "\n".join(steps_results)
 
 
-
-
 def write_to_doc_table(df, table_id, template_doc, output_doc):
     d = Document(template_doc)
     df_nb_row, df_nb_col = df.shape
@@ -89,6 +91,10 @@ def write_to_doc_table(df, table_id, template_doc, output_doc):
         d.tables[table_id].add_row()
         for col_idx in range(df_nb_col):
             d.tables[table_id].cell(table_row, col_idx).text = str(df.loc[df_row_idx][col_idx])
+
+    change_table_font(d.tables[table_id], 9)
+
+
     d.save(output_doc)
 
 def print_dataframe(df):
@@ -172,12 +178,15 @@ def get_last_valid_result(json_object):
             return [js_obj]
     return 0
 
+def my_http_request(url, user, password):
+    authentication = (user, password)
+    head = {"Content-Type": "application/json"}
+    return requests.get(url, auth=authentication, headers=head).json()
+
 def main():
     user_initial = create_user_initial()
     with open('config.yml') as f:
         config = yaml.safe_load(f)
-
-
 
     if config["test report"]:   # test report creation
         print("Process test Report")
@@ -191,12 +200,17 @@ def main():
         # df = create_df_from_json(js, ["Step", "Result Description", "Result", "Date", "Initial"],
         #                          [step_num, tr_result_description, tr_result, tr_date, tr_initial], 58746)
     else:   # test spec creation
-        with open(r"D:\user_data\Martin\OneDrive\Documents\json_response\get_cases\11362.json") as f:
-            js = json.load(f)
+        # with open(r"D:\user_data\Martin\OneDrive\Documents\json_response\get_cases\11362.json") as f:
+        #     js = json.load(f)
 
-        df = create_df_from_json(js, ["Step", "Description", "Requirement", "Expected Result",
+        for tr_section_id, table_id in config["table mapping"].items():
+            print("section : {}, table_id : {}".format(tr_section_id, table_id))
+            url = "https://testrail.dwos.com//index.php?/api/v2/get_cases/" + str(config["project id"]) + "&section_id=" + str(tr_section_id)
+            js = my_http_request(url, USER, PASSWORD)
+            df = create_df_from_json(js, ["Step", "Description", "Requirement", "Expected Result",
                                          "Test Method / Objective Evidence"],
                                  [step_num, tc_description, tc_requirement, tc_expected_result, tc_test_method])
+            write_to_doc_table(df, table_id, config["template path"], config["output doc name"])
 
 
     # df = create_df_from_json(tspec, ["Step", "Description", "Requirement", "Expected Result", "Test Method / Objective Evidence"],
@@ -205,9 +219,10 @@ def main():
     # write_to_doc_table(df, 3, r"D:\user_data\Martin\OneDrive - Straumann Group\Template\QUF73-2484 Template Test Specification (v1.3).docx",
     #                    "out.docx")
 
-    write_to_doc_table(df, 8,
-                       r"D:\user_data\Martin\OneDrive - Straumann Group\Template\QUF73-2485 Template Test Report (v1.0) (ID 3999).docx",
-                       "out.docx")
+
+
+                       # r"D:\user_data\Martin\OneDrive - Straumann Group\Template\QUF73-2485 Template Test Report (v1.0) (ID 3999).docx",
+                       # "out.docx")
 
 main()
 
