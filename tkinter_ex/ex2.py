21e073bfc3355df921c791c9a9a21ee0b1eb1e22
Martin Carufel <maccam6@gmail.com>
2023-04-28 11:24:47 -0400

    add widgets

--- tkinter_ex/ex2.py
+++ tkinter_ex/ex2.py
@@ -4,20 +4,33 @@ from tkinter import ttk
 class My_app(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.toto = tk.IntVar()
-        self.toto.set(0)
+        self.geometry("800x600")
+        self.tank1_value = tk.IntVar()
+        self.tank1_full_var = tk.BooleanVar()
+        self.tank1_value.set(0)
         self.tank = [{"id": 1, "name": "Tank 1", "capacity": 100, "level":0},
                      {"id": 2, "name": "Tank 2", "capacity": 200, "level":0}]
+        self.tank1_entry = 0
         # self.master = master
-        self.main_win = tk.Canvas(self)
-        self.main_win.pack()
+        self.main_win = tk.Canvas(self, width=800, height=400)
+        self.main_win.pack(padx=20, side=tk.LEFT)
         self.frame = ttk.Frame(self)
-        self.progressBar = ttk.Progressbar(self.main_win, mode='determinate', orient="vertical", length=200,
-                                           variable=self.toto)
-        self.progressBar.pack(padx=10, pady=10)
         self.frame.pack(padx=5, pady=5)
-        self.b1 = tk.Button(self, text="mod", command=lambda: self.tank_relative_level(1, 10))
-        self.b1.pack()
+        self.tank1_level = ttk.Progressbar(self.main_win, mode='determinate', orient="vertical", length=200,
+                                           variable=self.tank1_value)
+        self.tank1_level.grid(row=1, column=1, rowspan=3)
+        self.tank1_full = tk.Checkbutton(self.main_win, state="disabled",
+                        text="Full", variable=self.tank1_full_var)
+        self.tank1_full.grid(row=1, column=2, sticky=tk.W)
+        self.tank1_full_var.set(True)
+        self.tank1_level_label = tk.Label(self.main_win, textvariable=self.tank1_value)
+        self.tank1_level_label.grid(row=4, column=1)
+        # self.tank1_full_label = tk.Label(self.main_win, text="Full")
+        # self.tank1_full_label.grid(row=1, column=3, sticky=tk.W)
+        self.tank1_fill_button = tk.Button(self.main_win, text="Fill", command=lambda: self.fill_tank(1, self.tank1_entry))
+        self.tank1_fill_button.grid(row=2, column=2, padx=5)
+        self.tank1_entry = self.tank1_fill_value = tk.Entry(self.main_win)
+        self.tank1_fill_value.grid(row=2, column=3)
 
     def tank_relative_level(self, id, value):
         for tank_id in range(len(self.tank)):
@@ -26,8 +39,17 @@ class My_app(tk.Tk):
                 # self.progressBar["value"] = self.tank[tank_id]["level"]
                 # print(self.tank[tank_id]["level"])
                 # print(self.progressBar["value"])
-                self.toto.set(self.toto.get() + 10)
-                print(self.progressBar["value"])
+                self.tank1_value.set(self.tank1_value.get() + 10)
+
+    def fill_tank(self, tank, value):
+        value = int(self.tank1_value.get())
+        print(value)
+        if tank == 1:
+            if self.tank[tank-1]["level"] < self.tank[tank-1]["capacity"]:
+                self.tank[tank-1]["level"] = max(self.tank[tank-1]["level"] + value, self.tank[tank-1]["capacity"])
+                self.tank1_value.set(self.tank[tank-1]["level"])
+
+        pass
 
 
 
