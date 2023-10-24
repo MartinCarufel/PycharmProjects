d2eac114dc85da71d004a929a1baaf698e88b9a7
Martin Carufel <maccam6@gmail.com>
2023-10-24 07:13:36 -0400

    working on LG

--- MyWebDriver/main.py
+++ MyWebDriver/main.py
@@ -10,24 +10,14 @@ from time import sleep
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
-# create webdriver object
 driver = webdriver.Chrome(options=options)
 
-# get google.co.in
-# driver.get("https://google.com")
 driver.get("https://app.myhours.com/#/reports/dashboard")
-# element = driver.find_element(By.ID,"email")
+driver.maximize_window()
 element = driver.find_element(By.ID,"email")
 element.send_keys("maccam6@gmail.com")
-# element = driver.find_element(By.XPATH, "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/form[1]")
-element.send_keys(Keys.TAB)
-sleep(1)
-ActionChains(driver).send_keys("18,Mac&Amo").perform()
-sleep(2)
-
-# element.find_element(By.XPATH, r"//span[contains(text(),'Sign in')]")
-# element.click()
-# element.send_keys(Keys.ENTER)
-
+element = driver.find_element(By.ID, 'password')
+element.send_keys('18,Mac&Amo')
+driver.find_element(By.XPATH, "//span[contains(text(),'Sign in')]").click()
 
 
