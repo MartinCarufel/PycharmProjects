8607e9e24249b0bb38bbabe61be16d2e8c2f8788
Martin Carufel <maccam6@gmail.com>
2023-10-26 07:30:16 -0400

    add web site

--- MyWebDriver/Test_web_automation.py
+++ MyWebDriver/Test_web_automation.py
@@ -9,7 +9,7 @@ options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options)
 driver.implicitly_wait(10)
 driver.maximize_window()
-
+#  "https://www.softwaretestingmaterial.com/sample-webpage-to-automate/"
 driver.get("https://ultimateqa.com/simple-html-elements-for-automation/")
 sleep(2)
 l = len(driver.find_elements(By.XPATH, "//body[1]/div[1]/div[1]/div[1]/div[1]/article[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[2]/div[5]/div[1]/table[1]/tbody[1]/tr[1]/th[1]"))
