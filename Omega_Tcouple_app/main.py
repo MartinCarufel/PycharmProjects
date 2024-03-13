627fa982d35df0c16b8471988d9dc08913776b93
Martin Carufel <maccam6@gmail.com>
2024-03-13 15:57:41 -0400

    Add temp sensor Kivy project

---
+++ Omega_Tcouple_app/main.py
@@ -0,0 +1,21 @@
+import kivy.uix.button
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
+
+
+class MainWidget(Widget):
+    pass
+
+# class GridLayout(GridLayout):
+#     pass
+    """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)"""
+
+
+class ThermoCoupleOmegaApp(App):
+    pass
+
+
+ThermoCoupleOmegaApp().run()
