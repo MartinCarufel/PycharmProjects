9a6026754d17de925d1cee5edf8f09052ea1231f
Martin Carufel <maccam6@gmail.com>
2024-03-14 15:15:03 -0400

    Update Kivy test app

--- Omega_Tcouple_app/main.py
+++ Omega_Tcouple_app/main.py
@@ -7,14 +7,17 @@ from kivy.uix.gridlayout import GridLayout
 class MainWidget(Widget):
     pass
 
+class TopGrid(GridLayout):
+    pass
 # class GridLayout(GridLayout):
 #     pass
     """
     def __init__(self, **kwargs):
         super().__init__(**kwargs)"""
 
-
+# name the .kv file with same name without app. (Ex: ThermoCoupleOmega.kv)
 class ThermoCoupleOmegaApp(App):
+
     pass
 
 
