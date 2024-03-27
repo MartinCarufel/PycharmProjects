81035833a4628443db689fd6ee4bfe048fbf91de
Martin Carufel <maccam6@gmail.com>
2024-03-27 09:21:04 -0400

    Remove old code to logic sim

--- logic_sim/switch.py
+++ logic_sim/switch.py
@@ -49,38 +49,3 @@ class Switch_SPDT:
                 self.pin[2]['state'] = self.pin[3]['state']
             else:
                 self.pin[3]['state'] = self.pin[2]['state']
-
-class Switch2:
-    """Class defining ... """
-
-    def __init__(self, name):
-        """Class constructor"""
-        self.name = name
-        pin.pin.update({self.name+'a1':0, self.name+'y1':0})
-        self._state = False
-        threading.Thread(target=self.update).start()
-    # def get_input(self, key):
-    #     return self._input[key]
-
-    def set(self, key, value):
-        if value in (0, 1):
-            pin.pin[self.name+key] = value
-
-    def get(self, key):
-        return pin.pin[self.name+key]
-
-    def get_state(self):
-        return self._state
-
-    def set_state(self, value):
-        if value in (0, 1):
-            self._state = value
-
-    def update(self):
-        while True:
-            if self._state:
-                pin.pin[self.name+'y1'] = pin.pin[self.name+'a1']
-                # print(self._pin['y1'])
-
-            else:
-                pin.pin[self.name+'y1'] = None
