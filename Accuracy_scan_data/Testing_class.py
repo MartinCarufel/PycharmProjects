db4a851761237ba79622a105f5c33e4c2f45bb37
Martin Carufel <maccam6@gmail.com>
2024-05-24 16:22:33 -0400

    Add data analyser for scan accuracy

--- Accuracy_scan_data/Testing_class.py
+++ Accuracy_scan_data/Testing_class.py
@@ -23,4 +23,18 @@ class data_test(unittest.TestCase):
 
     def test_col_RMS(self):
         df = self.data_class.read_csv("./Test_data/batchMetrologySummary.csv")
-        print(df["RMS"])
+        # print(df["RMS"])
+        for dt in df["RMS"]:
+            print(dt)
+
+    def test_range(self):
+        range_band = [0, 0.005, 0.010, 0.015, 0.020, 0.025, 0.030, 0.035, 0.040, 0.045, 0.050, 0.055, 0.060, 0.065,
+                           0.070, 0.075, 0.080]
+        self.range_band_count = []
+        df = self.data_class.read_csv("./Test_data/batchMetrologySummary.csv")
+        data_table = []
+        data_expected_table = [0, 0, 0, 2, 6, 2, 0, 0, 0, 0, 0, 0, 0, 0]
+        for i in range(len(range_band)-1):
+            data_table.append(self.data_class.value_within_range_count(range_band[i], range_band[i+1], df['RMS']))
+            pass
+
--- Accuracy_scan_data/scan_data.py
+++ Accuracy_scan_data/scan_data.py
@@ -13,6 +13,24 @@ class Data_analyser:
         df = pd.read_csv(file_path, skiprows=5, nrows=10, names= header)
         return df
 
+    def value_within_range_count(self, range_min, range_max, df_col):
+        """
+
+        :param range_min:
+        :param range_max:
+        :param data: dataframe colum to be analysed ex: df['max']
+        :return:
+        """
+        count = 0
+        # print(f'range_min: {range_min}')
+        # print(f'range_max: {range_max}')
+        for data in df_col:
+            # print(f'Data analisé: {data}')
+            if range_min < data <= range_max:
+                count = count + 1
+        return count
+
+
 
 
 
