03f896679327ac961b22b9baee898414aecfc041
Martin Carufel <maccam6@gmail.com>
2024-07-25 09:39:58 -0400

    add display function, add export to csv

--- IOS Processing time/main.py
+++ IOS Processing time/main.py
@@ -1,17 +1,21 @@
 
 import regex
 from tkinter import filedialog
+from datetime import datetime
 class Process_time:
 
     def __init__(self):
+        self.file_path = filedialog.askopenfilename()
         self.start_stop_time = None
+        self.start_process_line_number = []
+        self.stop_process_time_list = []
         pass
 
-    def get_start_process_time(self, file_path):
+    def get_start_process_time(self):
         start_process_time_list = []
         start_regex_pattern = "IOBridgeThread - startPostProcessing"
         compile_regex_start = regex.compile(start_regex_pattern)
-        with open(file_path, mode='r') as f:
+        with open(self.file_path, mode='r') as f:
             lines = f.readlines()
             for line in lines:
                 if compile_regex_start.search(line) != None:
@@ -21,43 +25,41 @@ class Process_time:
                     pass
         return start_process_time_list
 
-    def get_start_process_line_number(self, file_path):
-        start_process_line_number = []
+    def get_start_process_line_number(self):
         start_regex_pattern = "IOBridgeThread - startPostProcessing"
         compile_regex_start = regex.compile(start_regex_pattern)
-        with open(file_path, mode='r') as f:
+        with open(self.file_path, mode='r') as f:
             lines = f.readlines()
             for i in range(len(lines)):
                 if compile_regex_start.search(lines[i]) != None:
-                    start_process_line_number.append(i)
+                    self.start_process_line_number.append(i)
                     pass
                 else:
                     pass
-        return start_process_line_number
+        return self.start_process_line_number
 
-    def get_next_switch_view(self, file_path, start_list):
-        start_list = start_list.copy()
-        stop_process_time_list = []
+    def get_next_switch_view(self):
+        start_list = self.start_process_line_number.copy()
         # stop_regex_pattern = "Post processing popup closed"
         stop_regex_pattern = "View entered : Review view"
         compile_regex_stop = regex.compile(stop_regex_pattern)
-        with open(file_path, mode='r') as f:
+        with open(self.file_path, mode='r') as f:
             lines = f.readlines()
             start_list.append(len(lines))
             for st_list_id in range(len(start_list)-1):
                 for line_id in range(start_list[st_list_id], start_list[st_list_id+1], +1):
                     if compile_regex_stop.search(lines[line_id]) != None:
-                        stop_process_time_list.append(line_id)
+                        self.stop_process_time_list.append(line_id)
                         break
-        return stop_process_time_list
+        return self.stop_process_time_list
 
 
-    def get_stop_process_time(self, file_path):
+    def get_stop_process_time(self):
         stop_process_time_list = []
         # stop_regex_pattern = "Post processing popup closed"
         stop_regex_pattern = "View exited : Review view"
         compile_regex_stop = regex.compile(stop_regex_pattern)
-        with open(file_path, mode='r') as f:
+        with open(self.file_path, mode='r') as f:
             lines = f.readlines()
             for line in lines:
                 if compile_regex_stop.search(line) != None:
@@ -97,10 +99,25 @@ class Process_time:
                          stop_minute + stop_second) - (start_hour * 3600 + start_minute * 60 + start_second )
         return proc_time
 
-    def get_line_time_stamp(self, file_path, line_no):
-        with open(file_path, 'r') as f:
+    def get_line_time_stamp(self, line_no):
+        with open(self.file_path, 'r') as f:
             lines = f.readlines()
             return lines[line_no][0:14]
+
+    def display_process_time(self, process_time_list):
+        print('Process start line number, Process end line number, process time (s)')
+        for i in range(len(process_time_list)):
+            print(f'{self.start_process_line_number[i]+1},{self.stop_process_time_list[i]+1},{process_time_list[i]}')
+
+    def export_to_csv(self, process_time_list):
+        now = datetime.now()
+        formatted_now = now.strftime("%Y-%m-%d_%H%M%S")
+        with open(f"Process_time_{formatted_now}.csv", 'w') as f:
+            f.writelines('Process start line number, Process end line number, process time (s)\n')
+
+            for i in range(len(process_time_list)):
+                f.writelines(f'{self.start_process_line_number[i]+1},{self.stop_process_time_list[i]+1},{process_time_list[i]}\n')
+
 
 
 def main():
@@ -118,19 +135,18 @@ def main():
 def main2():
     process_time_calc = []
     pt = Process_time()
-    path = filedialog.askopenfilename()
+    # path = filedialog.askopenfilename()
     # path = r"C:\Users\u120230\git\PycharmProjects\IOS Processing time\Test data\IOClient.txt_05-28-2024_13-23-42.617.txt"
 
-    start_proc_line_number_list = pt.get_start_process_line_number(path)
-    stop_proc_line_number_list = pt.get_next_switch_view(path, start_proc_line_number_list)
-    print(start_proc_line_number_list)
-    print(stop_proc_line_number_list)
+    start_proc_line_number_list = pt.get_start_process_line_number()
+    stop_proc_line_number_list = pt.get_next_switch_view()
     start_stop_list = pt.create_start_stop_tupple_list(start_proc_line_number_list, stop_proc_line_number_list)
 
     for start, stop in start_stop_list:
-        process_time_calc.append(pt.process_time_calculation(pt.get_line_time_stamp(path, start),
-                                                             pt.get_line_time_stamp(path, stop)))
-    print(process_time_calc)
+        process_time_calc.append(pt.process_time_calculation(pt.get_line_time_stamp(start),
+                                                             pt.get_line_time_stamp(stop)))
+    pt.display_process_time(process_time_calc)
+    pt.export_to_csv(process_time_calc)
 
 
 
