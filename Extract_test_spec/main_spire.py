e38fc7e6fdb143e6dad7b7677ccfdf92be9834fc
Martin Carufel <maccam6@gmail.com>
2025-04-15 13:08:12 -0400

    add csv export file

--- Extract_test_spec/main_spire.py
+++ Extract_test_spec/main_spire.py
@@ -1,4 +1,5 @@
 import re
+import string
 from spire.doc import *
 from spire.doc.common import *
 
@@ -38,7 +39,7 @@ def fetch_doc_in_dict(document):
                                             para = cell.Paragraphs.get_Item(p)
                                             para_text = para.Text.strip().replace('\r', '\n').replace('\v', '\n')
                                             cell_lines.append(para_text)
-                                        cell_text = '\n'.join(cell_lines)
+                                        cell_text = '\\n'.join(cell_lines)
                                         row_data.append(cell_text)
                                     table_data.append(row_data)
 
@@ -46,6 +47,31 @@ def fetch_doc_in_dict(document):
                             break  # Stop looking once we found the next table
             j += 1
     return tc_tables
+
+
+
+def csv_construct_header(header_l):
+    return ";".join(header_l) + "\n"
+
+
+def csv_construct_tc(table):
+    return f";\"Test Case\"; \"{table}\";;;\n"
+
+def csv_construct_test_step(id, text):
+    return f";;;\"{id}\";\"{text}\"\\n\\n"
+
+
+def csv_construct_req(text):
+    return f"\"Requirement(s): {text}\";"
+    pass
+
+
+def csv_construct_exp_res(text):
+    return f"\"{text}\"\\n\\n"
+
+
+def csv_construct_TM_OE(text):
+    return f"TM / OE:\"{text}\"\n"
 
 
 def debug_print(tc_tables):
@@ -57,14 +83,35 @@ def debug_print(tc_tables):
             print(row)
 
 
+
 def main():
     # Load the document
     document = Document()
     document.LoadFromFile("DEV-0044600 STMN IOS Main Application Verification Specifications Rev 4.docx")
     # Dictionary to hold TC codes and their following tables
     tc_tables = fetch_doc_in_dict(document)
-    debug_print(tc_tables)
+    # debug_print(tc_tables)
     document.Close()
+    print(tc_tables)
+    with open("export.csv", mode="w", encoding="UTF-8") as f:
+        f.write(csv_construct_header(["ID","Work Item Type","Title","Test Step","Step Action","Step Expected"]))
+        # Cycle test case
+        for tc, table in tc_tables.items():
+            f.write(csv_construct_tc(table[0]))
+
+            # cycle test step in test case
+            for i in range(1, len(table)):
+                f.write(csv_construct_test_step(i, table[i][1]))
+                f.write(csv_construct_req(table[i][2]))
+                f.write(csv_construct_exp_res(table[i][3]))
+                f.write(csv_construct_TM_OE(table[i][4]))
+                # print(table[i][2])
+                pass
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
